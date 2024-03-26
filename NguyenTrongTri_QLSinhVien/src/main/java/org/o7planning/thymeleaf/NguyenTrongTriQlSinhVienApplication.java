81df91abe940f7653612f3ea75132fe0e7f0c457
tri63132732 <144207384+tri63132732@users.noreply.github.com>
2024-03-26 15:12:54 +0700

    update

---
+++ NguyenTrongTri_QLSinhVien/src/main/java/org/o7planning/thymeleaf/NguyenTrongTriQlSinhVienApplication.java
@@ -0,0 +1,13 @@
+package org.o7planning.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenTrongTriQlSinhVienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenTrongTriQlSinhVienApplication.class, args);
+	}
+
+}
